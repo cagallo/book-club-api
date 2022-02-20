4aa6c9fabcf0955551990c289c37e213add316f8
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-20 10:45:14 -0800

    Create getSingleBook query

--- db/queries.js
+++ db/queries.js
@@ -4,5 +4,9 @@ const database = require('knex')(connection);
 module.exports = {
     getAll() {
         return database('books')
+    },
+    getSingleBook(request) {
+        return database('books').where('id', request.params.id).select();
     }
+
 }
