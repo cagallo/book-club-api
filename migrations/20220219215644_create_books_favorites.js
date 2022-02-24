7dba3a26c842f49a9b0e80bdd3c506ea0e2b1544
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-23 23:23:05 -0700

    Clean comments

--- migrations/20220219215644_create_books_favorites.js
+++ migrations/20220219215644_create_books_favorites.js
@@ -1,7 +1,3 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
   return knex.schema.createTable('books', (table) => {
     table.increments('id').primary();
--- migrations/20220220215434_create_timestamp_property_favorites.js
+++ migrations/20220220215434_create_timestamp_property_favorites.js
@@ -1,17 +1,9 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
   return knex.schema.table('favorites', (table) => {
     table.timestamps(true, true);
   });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
   return knex.schema.table('favorites', (table) => {
     table.dropTimestamps();
