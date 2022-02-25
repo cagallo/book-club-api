c46818e4fbbe812fa6fa8e9ba467309a280d329d
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-02-24 19:12:59 -0700

    Added patch to queries

--- queries.js
+++ queries.js
@@ -7,15 +7,20 @@ module.exports = {
       return database('books');
     },
     getSingleBook(request) {
-      return database('books').where('id', request.params.id).select();
+      return database('books').where('isbn', request.params.isbn).select();
     },
     addBookToFavs(favorite) {
       return database('favorites').insert(favorite, 'id');
     },
     removeBookFromFavs(request) {
-      return database('favorites').where('id', request.params.id).del();
+      return database('favorites').where('isbn', request.params.isbn).del();
     },
     getAllFavs() {
       return database('favorites');
+    },
+    updateFavorited(request) {
+      console.log("isbn -------->", request.params.isbn);
+      console.log("REQUEST BODY======>", JSON.stringify(request.body));
+      return database('books').where('isbn', request.params.isbn).update({isFavorited: request.body.isFavorited});
     }
 }
